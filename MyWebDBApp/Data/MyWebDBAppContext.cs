eecaa73633e41a8a0708b30280ec108d088f3aad
Mohammad Abu Snober <mohammad.abusnober@hotmail.com>
2024-05-15 18:15:40 +0300

    Add project files.

---
+++ MyWebDBApp/Data/MyWebDBAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyWebDBApp.Models;
+
+namespace MyWebDBApp.Data
+{
+    public class MyWebDBAppContext : DbContext
+    {
+        public MyWebDBAppContext (DbContextOptions<MyWebDBAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyWebDBApp.Models.Clients> Clients { get; set; } = default!;
+    }
+}
---
+++ MyWebDBApp/Models/Clients.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebDBApp.Models
+{
+    public class Clients
+    {
+        [Required]
+        [RegularExpression(@"[0-9]{4}")]
+        public int Id { set; get; }
+        
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string mobile { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        public int points { get; set; }
+
+        public string comments { get; set; }
+
+    }
+}
